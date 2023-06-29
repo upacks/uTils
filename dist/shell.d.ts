feb2b5cde395607e8e83494a927699a27507200c
tulgaewk <tulgaewk@users.noreply.github.com>
2023-06-29 01:00:49 +0000

    deploy: 809f3891d8458148457bc66efe53d09b9abce316

---
+++ dist/shell.d.ts
@@ -0,0 +1,5 @@
+import shell from 'shelljs';
+export declare const Shell: typeof shell;
+export declare const CommandExists: (command: string) => boolean;
+export declare const PackageExists: (name: string) => boolean;
+//# sourceMappingURL=shell.d.ts.map
