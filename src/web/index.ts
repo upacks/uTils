0eff63e773723287f6ec677f0de21894f72bdbce
Gantulga Khuyag <gantulgak@ot.mn>
2023-08-22 08:38:10 +0800

    Add web

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,1 +1,23 @@
 export * from './log'
+
+export const help = () => {
+
+    import('./log').then(({ log }) => {
+
+        log.success(`log.success('***')`)
+        log.info(`log.info('***')`)
+        log.warn(`log.warn('***')`)
+        log.error(`log.error('***')`)
+
+        log.req(`log.req('***')`)
+        log.res(`log.res('***')`)
+
+    })
+
+}
+
+if (typeof process !== 'undefined' && process.argv.includes('--debug')) {
+
+    help()
+
+}
