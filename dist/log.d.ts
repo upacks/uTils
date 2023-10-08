26b9f739baeb3487e542fa7235d6a9bfbd5dfed6
Gantulga-kh <Gantulga-kh@users.noreply.github.com>
2023-10-08 07:32:41 +0000

    deploy: 4194451fa46568e86ae3cd82403fa64a2c6f621d

---
+++ dist/log.d.ts
@@ -0,0 +1,9 @@
+export declare const log: {
+    req: (message: string) => boolean;
+    res: (message: string) => boolean;
+    success: (message: string) => boolean;
+    info: (message: string) => boolean;
+    warn: (message: string) => boolean;
+    error: (message: string) => boolean;
+};
+//# sourceMappingURL=log.d.ts.map
---
+++ dist/web/index.d.ts
@@ -0,0 +1,6 @@
+export * from './collectors';
+export * from './execution';
+export * from './utils';
+export * from './log';
+export declare const help: () => void;
+//# sourceMappingURL=index.d.ts.map
