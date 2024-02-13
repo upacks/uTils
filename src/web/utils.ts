db32471ddf52730d48ed532e193a6959cca77718
Gantulga Khuyag <gantulgak@ot.mn>
2024-02-13 08:56:17 +0800

    v1.1.10

--- src/web/utils.ts
+++ src/web/utils.ts
@@ -107,3 +107,24 @@ export const KeyValue = (key: string, value: any = undefined): string => {
     } catch { return '' }
 
 }
+
+/**
+ * JWT to JSON only works on browser
+ * @param token
+ * @returns 
+ */
+export const parseJwt = (token: string) => {
+
+    try {
+
+        var base64Url = token.split('.')[1]
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+        }).join(''))
+
+        return JSON.parse(jsonPayload) ?? null
+
+    } catch (err) { return null }
+
+}
