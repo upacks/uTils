b1fb0239a44fd3a9f7ae41240d1fcd7a669864e8
gantulgak <gantulgak@riotinto.com>
2024-04-14 11:38:04 +0800

    v1.1.12

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,13 @@ export * from './object'
 export * from './utils'
 export * from './shell'
 export * from './log'
+
+/* setTimeout(() => {
+
+    const key = `TRX_${short()}`
+    ulog(key, 'req', 'Pulling data')
+    setTimeout(() => { ulog(key, 'info', 'Preparing for response') }, 1500)
+    setTimeout(() => { ulog(key, 'res', 'Data from server') }, 1700)
+    setTimeout(() => { ulog(key, 'catch', 'Succeeded') }, 2500)
+
+}, 500) */
--- src/log.ts
+++ src/log.ts
@@ -132,3 +132,41 @@ export class Log {
     }
 
 }
+
+/** Extra ungly logging **/
+
+const keys: any = {}
+const icon = {
+    'cloud': '🌍',
+    'vehicle': '🚂',
+    'user': '👩‍💻',
+    'db': '💾',
+}
+type tUlog = 'req' | 'res' | 'info' | 'then' | 'catch'
+type tDirection = 'cloud' | 'vehicle' | 'user' | 'db'
+setInterval(() => { for (const key in keys) if (typeof keys[key] === 'object' && (Date.now() - keys[key].start) > 30000) delete keys[key] }, 1000)
+export const ushort = () => `${Date.now()}`.substring(7, 13)
+export const ulog = (key: string, type: tUlog, message: string, from: tDirection = 'vehicle', to: tDirection = 'cloud') => {
+
+    let time = '0.00s', _fr = icon[from], _to = icon[to]
+
+    if (typeof keys[key] === 'undefined') keys[key] = { start: Date.now(), logs: [] }
+    if (typeof keys[key] === 'object') {
+        time = `${((Date.now() - keys[key].start) / 1000).toFixed(2)}s`
+        // keys[key].start = Date.now() Show difference from prev.time
+    }
+
+    type === 'req' && keys[key].logs.push(cl.cyanBright(`${_fr} ⇒ ${_to} ${time} [${key}] ${message}`))
+    type === 'res' && keys[key].logs.push(cl.cyanBright(`${_fr} ⇒ ${_to} ${time} [${key}] ${message}`))
+    type === 'info' && keys[key].logs.push(cl.white(`${_fr} ⚋ ${_to} ${time} [${key}] ${message}`))
+    type === 'then' && keys[key].logs.push(cl.green(`${_fr} ✔ ${_to} ${time} [${key}] ${message}`))
+    type === 'catch' && keys[key].logs.push(cl.red(`${_fr} ✗ ${_to} ${time} [${key}] ${message}`))
+
+    if (type === 'then' || type === 'catch') {
+        for (const chlk of keys[key].logs) console.log(chlk)
+        delete keys[key]
+    }
+
+    return true
+
+}
